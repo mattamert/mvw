1e9d1c776e4ca7bcf171600b780e62df39c2bab5
Matt Amert <mattamert@gmail.com>
2020-05-17 11:42:59 -0700

    Cleaned up main.cpp

--- d3d12/main.cpp
+++ d3d12/main.cpp
@@ -2,65 +2,7 @@
 
 #include "d3d12/DXWindow.h"
 
-#define IDT_TIMER1 1234
-
-LRESULT CALLBACK WndProc(HWND hwnd,
-                         UINT message,
-                         WPARAM wParam,
-                         LPARAM lParam) {
-  DXWindow* app =
-      reinterpret_cast<DXWindow*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-  switch (message) {
-    case WM_CREATE: {
-      LPCREATESTRUCT createStruct = (LPCREATESTRUCT)lParam;
-      DXWindow* app = (DXWindow*)createStruct->lpCreateParams;
-      SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)app);
-
-      SetTimer(hwnd,              // handle to main window
-               IDT_TIMER1,        // timer identifier
-               16,                // 10-second interval
-               (TIMERPROC)NULL);  // no timer callback
-
-      return 0;
-    }
-
-    case WM_TIMER:
-      if (wParam == IDT_TIMER1) {
-        InvalidateRect(hwnd, nullptr, false);
-      }
-      return 0;
-
-    case WM_SIZE:
-      if (app != nullptr) {
-        UINT width = LOWORD(lParam);
-        UINT height = HIWORD(lParam);
-        app->OnResize(width, height);
-      }
-      return 0;
-
-    case WM_DISPLAYCHANGE:
-      if (app != nullptr) {
-        InvalidateRect(hwnd, nullptr, false);
-      }
-      return 0;
-
-    case WM_PAINT:
-      if (app != nullptr) {
-        //app->Animate();
-        app->DrawScene();
-        app->Present();
-        ValidateRect(hwnd, nullptr);
-      }
-      return 0;
-
-    case WM_DESTROY:
-      PostQuitMessage(0);
-      return 0;
-
-    default:
-      return DefWindowProc(hwnd, message, wParam, lParam);
-  }
-}
+#define USE_CONSOLE_SUBSYSTEM
 
 void RunMessageLoop() {
   MSG msg;
@@ -70,23 +12,7 @@ void RunMessageLoop() {
   }
 }
 
-// int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
-// LPSTR /* lpCmdLine */, int /*nCmdShow*/)
-//{
-//    HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
-//
-//    if (SUCCEEDED(CoInitialize(NULL)))
-//    {
-//        {
-//            App app;
-//            app.Initialize();
-//            RunMessageLoop();
-//        }
-//        CoUninitialize();
-//    }
-//
-//    return 0;
-//}
+#ifdef USE_CONSOLE_SUBSYSTEM
 
 int main() {
   HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
@@ -102,3 +28,25 @@ int main() {
 
   return 0;
 }
+
+#else
+
+int WINAPI WinMain(HINSTANCE /*hInstance*/,
+                   HINSTANCE /*hPrevInstance*/,
+                   LPSTR /* lpCmdLine */,
+                   int /*nCmdShow*/) {
+  HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+  if (SUCCEEDED(CoInitialize(NULL))) {
+    {
+      DXWindow app;
+      app.Initialize();
+      RunMessageLoop();
+    }
+    CoUninitialize();
+  }
+
+  return 0;
+}
+
+#endif
