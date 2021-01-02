bf8a40314fb24d43da4f10a3249c3a00ef81da5c
Matt Amert <mattamert@gmail.com>
2021-01-02 12:59:18 -0800

    Cleaned up shader a bit

--- d3d12/ColorPassShaders.hlsl
+++ d3d12/ColorPassShaders.hlsl
@@ -16,7 +16,7 @@ struct PSInput {
   float4 position : SV_POSITION;
   float2 tex : TEXCOORD;
   float3 normal : NORMAL;
-  float4 shadowMapTexCoord : TEXCOORD1;
+  float4 shadowMapPos : TEXCOORD1;
 };
 
 PSInput VSMain(float3 pos : POSITION, float2 tex : TEXCOORD, float3 normal : NORMAL) {
@@ -27,21 +27,20 @@ PSInput VSMain(float3 pos : POSITION, float2 tex : TEXCOORD, float3 normal : NOR
   result.position = mul(modelViewProjection, float4(pos, 1.f));
   result.tex = tex;
   result.normal = normal;
-  result.shadowMapTexCoord = mul(shadowCameraModelViewProjection, float4(pos, 1.f));
+  result.shadowMapPos = mul(shadowCameraModelViewProjection, float4(pos, 1.f));
   return result;
 }
 
-float4 PSMain(PSInput input) : SV_TARGET{
+float4 PSMain(PSInput input) : SV_TARGET {
   float4 texValue = objectTexture.Sample(aniSampler, input.tex);
   if (texValue.w == 0.f) {
     discard;
   }
 
-  input.shadowMapTexCoord.xyz /= input.shadowMapTexCoord.w;
-
-  float currentDepth = input.shadowMapTexCoord.z;
-  float visibleDepth = shadowMap.Sample(pointClamp, float2((input.shadowMapTexCoord.x + 1) / 2, 1 - (input.shadowMapTexCoord.y + 1) / 2));
-  if (currentDepth > visibleDepth)
+  float2 shadowMapTexCoord = float2((input.shadowMapPos.x + 1) / 2, 1 - (input.shadowMapPos.y + 1) / 2);
+  float depthInShadowMap = input.shadowMapPos.z;
+  float visibleDepthFromShadowMap = shadowMap.Sample(pointClamp, shadowMapTexCoord);
+  if (depthInShadowMap > visibleDepthFromShadowMap)
     texValue.xyz *= 0.5;
 
   return texValue;
