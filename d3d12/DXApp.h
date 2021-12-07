7d080ce6061002435a25f7f29dc9afa4de2fc363
Matt Amert <mattamert@gmail.com>
2021-12-07 12:33:59 -0800

    Removed unused includes from DXApp.h

--- d3d12/DXApp.h
+++ d3d12/DXApp.h
@@ -1,24 +1,11 @@
 #pragma once
 
 #include <Windows.h>
-#include <d3d12.h>
-#include <dxgi1_4.h>
-#include <wrl/client.h>  // For ComPtr
 
-#include <cstdint>
 #include <memory>
 
-#include "d3d12/Animation.h"
-#include "d3d12/Camera.h"
-#include "d3d12/ConstantBufferAllocator.h"
 #include "d3d12/D3D12Renderer.h"
-#include "d3d12/DescriptorHeapManagers.h"
-#include "d3d12/Object.h"
-#include "d3d12/Pass.h"
-#include "d3d12/ResourceGarbageCollector.h"
 #include "d3d12/Scene.h"
-#include "d3d12/TextureResources.h"
-#include "d3d12/WindowSwapChain.h"
 
 class MessageQueue;
 
